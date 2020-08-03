612584526a4c00cb01437de81bb532091fb2d1bf
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-08-03 22:19:27 +0530

    db changes into db.sqlite3

--- magazine/admin_form/models.py
+++ magazine/admin_form/models.py
@@ -32,9 +32,6 @@ class All_Fields(models.Model):
 class Image(models.Model):
     img = models.ImageField(upload_to='images', blank=True)
 
-    def __str__(self):
-        return self.img
-
 class Subscription(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField()
--- magazine/magazine/settings.py
+++ magazine/magazine/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'magazine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'magazine',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
